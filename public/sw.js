8eb67c0bd20de8600cffd9372b64049f24fff189
RogueGhost93 <vasaladacki@tutanota.com>
2026-04-13 22:05:45 +0200

    Mealie bug fixes

--- public/sw.js
+++ public/sw.js
@@ -12,9 +12,9 @@
  *   API: Always network (no caching of user data)
  */
 
-const SHELL_CACHE   = 'planner-shell-v98';
-const PAGES_CACHE   = 'planner-pages-v98';
-const ASSETS_CACHE  = 'planner-assets-v98';
+const SHELL_CACHE   = 'planner-shell-v99';
+const PAGES_CACHE   = 'planner-pages-v99';
+const ASSETS_CACHE  = 'planner-assets-v99';
 const ALL_CACHES    = [SHELL_CACHE, PAGES_CACHE, ASSETS_CACHE];
 
 // App shell: needed immediately for first render
