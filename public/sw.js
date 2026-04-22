ac312edda2b4243fe6f56e73b787d0fe269ee736
RogueGhost93 <vasaladacki@tutanota.com>
2026-04-22 14:42:33 +0200

    fix scroll bar gutter

--- public/sw.js
+++ public/sw.js
@@ -12,9 +12,9 @@
  *   API: Always network (no caching of user data)
  */
 
-const SHELL_CACHE   = 'planium-shell-v111';
-const PAGES_CACHE   = 'planium-pages-v111';
-const ASSETS_CACHE  = 'planium-assets-v111';
+const SHELL_CACHE   = 'planium-shell-v113';
+const PAGES_CACHE   = 'planium-pages-v113';
+const ASSETS_CACHE  = 'planium-assets-v113';
 const ALL_CACHES    = [SHELL_CACHE, PAGES_CACHE, ASSETS_CACHE];
 
 // App shell: needed immediately for first render
